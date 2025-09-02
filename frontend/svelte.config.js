331afdb7dc5f904ef6e79e582f3755147271a086
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-09-02 13:42:53 -0400

    build pages

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -7,7 +7,9 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+			pages: 'build',
+			assets: 'build'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
